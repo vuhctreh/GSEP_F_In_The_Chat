8c8248e5cea5aeff3c050fc6bc69721949a89316
William-Droin <wdroin@me.com>
2021-03-01 14:47:54 +0100

    bug correction
    if level was above 9 the page was not loading.

--- app/small_scripts_def.py
+++ app/small_scripts_def.py
@@ -7,23 +7,28 @@ def check_points_treshold(total_points):
     level = total_points/50
 
     if (level >= 9):
-        return list_coffee[9]
+        return 9
 
     else:
         return level
 
 #william
 def how_much_to_go(current_level):
-    # transforming the current_level which is a float into an int
-    int_value = int(current_level)
 
-    # adding 1 to the int value to get the next threshold
-    # take away from the next threshold the current level
-    # this will give the level missing to go to the next threshold
-    to_go_level = (int_value+1) - current_level
+    if (current_level<9):
+        # transforming the current_level which is a float into an int
+        int_value = int(current_level)
 
-    # multiplying it by the 50 (this value has to be the same as the divider in the function check_points_treshold)
-    # this gives the number of points to go before the next treshold
-    to_go_points = to_go_level*50
+        # adding 1 to the int value to get the next threshold
+        # take away from the next threshold the current level
+        # this will give the level missing to go to the next threshold
+        to_go_level = (int_value+1) - current_level
 
-    return to_go_points
+        # multiplying it by the 50 (this value has to be the same as the divider in the function check_points_treshold)
+        # this gives the number of points to go before the next treshold
+        to_go_points = to_go_level*50
+
+        return to_go_points
+
+    else:
+        return 0
