40905c0a23b275ab176fe5013ae92ca5f7347894
William Droin <72973649+William-Droin@users.noreply.github.com>
2021-03-03 14:57:46 +0100

    updated the import of the corseheaders
    the module was not imported correctly in middle ware and rendered the deployment impossible.

--- pythondjangoapp/settings/base.py
+++ pythondjangoapp/settings/base.py
@@ -32,7 +32,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'pythondjangoapp.urls'
