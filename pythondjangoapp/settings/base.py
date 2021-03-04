5aae60b1bf172535e3779303ee7d864d9011e2de
isebire <igssloth@gmail.com>
2021-03-04 14:50:27 +0000

    settings fix - sorry

--- pythondjangoapp/settings/base.py
+++ pythondjangoapp/settings/base.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'livereload',
     'jquery',
-    'app'
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -31,7 +31,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'pythondjangoapp.urls'
