9d2d5db9bbf4ed7c3b8b2551dffbb73dad9b4a44
Andrei <andrei_lazar_24@yahoo.com>
2021-03-10 13:53:29 +0000

    More testing

--- app/tests.py
+++ app/tests.py
@@ -2,19 +2,9 @@
 
 from django.test import SimpleTestCase, TestCase, Client
 from django.template.loader import render_to_string
-from app.models import CafeTable, CoffeeUser, Message, Task
+from app.models import CafeTable, CoffeeUser, Message, Report, Task
 
 client = Client()
-
-
-class HealthEndpointTests(SimpleTestCase):
-    """ Placeholder """
-
-    def test_health_status_is_up(self):
-        """ Placeholder """
-        response = self.client.get('/health')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '{"status": "UP"}')
 
 
 class LogInTests(TestCase):
@@ -130,7 +120,6 @@ class InTableTests(TestCase):
         response = self.client.post('/tables/1', data)
         response_html = response.content.decode()
         self.assertTrue(Message.objects.exists())
-        self.assertTrue('Test msg' in response_html)
 
     def test_new_topic_empty_post_data(self):
         """ Placeholder """
@@ -230,6 +219,25 @@ class SetTaskTests(TestCase):
         """ Placeholder """
         response = self.client.get('/set_tasks')
         self.assertContains(response, 'csrfmiddlewaretoken')
+    
+    def test_template(self):
+        response = self.client.get('/set_tasks')
+        self.assertTemplateUsed(response, 'set_tasks.html')
+
+    def task_in_db_test(self):
+        data = {
+            'task_name': 'Test-213943',
+            'created_by': '',
+            'table_id': '',
+            'task_content': '',
+            'points': '',
+            'recurrence_interval': '',
+            'max_repeats': '',
+        }
+
+        response = self.client.post('/set_tasks', data)
+
+        self.assertTrue(Task.objects.filter(task_name='Test-213943').exists())
 
 
 class ViewTaskTests(TestCase):
@@ -250,8 +258,91 @@ class ViewTaskTests(TestCase):
         task = Task.objects.create(task_name="tasktest", table_id=table,
                                    created_by=user, task_content="lol",
                                    points=1)
+        task_should_not_be_displayed = Task.objects.create(task_name="tasktestNO", table_id=table2,
+                                   created_by=user, task_content="lol",
+                                   points=1)
 
     def test_status_code(self):
-        """ Placeholder """
+        """ Tests if the page is reachable """
         response = self.client.get('/view_tasks')
+        self.assertEqual(response.status_code, 200)
+    
+    def task_exists_in_db_test(self):
+        self.assertTrue(Report.objects.count(), 1)
+    
+    def template_connection_test(self):
+        response = self.client.get('/view_tasks')
+        self.assertTemplateUsed(response, 'view_tasks.html')
+    
+    def right_tasks_displayed_test(self):
+        response = self.client.get('/view_tasks')
+        self.assertContains(response, 'tasktest')
+        self.assertNotContains(response, 'tasktestNO')
+        # Tasks only from the correct tables are displayed
+
+class ReportingTests(TestCase):
+
+    def setUp(self):
+        table = CafeTable.objects.create(table_id='Test',
+                                         university='Test uni')
+        table2 = CafeTable.objects.create(table_id='Test 2',
+                                          university='Test uni')
+        self.user = CoffeeUser.objects.create_user(
+            email='test@test.com', first_name='testf', last_name='testl',
+            university='Test uni', is_staff=False, password='123'
+        )
+        self.user.cafe_table_ids.add(table)
+        self.client.login(email='test@test.com', password='123')
+    
+    def test_status_code(self):
+        """ Tests if the page is reachable """
+        response = self.client.get('/report')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'report.html')
+
+    def test_csrf(self):
+        """ Placeholder """
+        response = self.client.get('/report')
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def create_report_test(self):
+        data = {
+            'title': 'TestReport-213943',
+            'category': '',
+            'detail': '',
+            'table_id': '',
+            'flagged_by': '',
+        }
+        
+        self.assertTrue(Report.objects.count(), 0)
+
+        response = self.client.post('/report', data)
+
+        # Check request is successful
+        self.assertTrue(response.status_code, 200)
+
+        # Check object created in database
+        self.assertTrue(Report.objects.count(), 1)
+        self.assertTrue(Report.objects.filter(title='TestReport-213943').exists())
+
+
+class HealthEndpointTests(SimpleTestCase):
+    """ Placeholder """
+
+    def test_health_status_is_up(self):
+        """ Placeholder """
+        response = self.client.get('/health')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '{"status": "UP"}')
+
+class PrivacyAndTermsTests(SimpleTestCase):
+    """
+    Tests the Terms and Conditions and Privacy Policy pages to ensure they're up and working
+    """
+    def privacy_policy_status_is_up(self):
+        response = self.client.get('/privacy')
+        self.assertEqual(response.status_code, 200)
+    
+    def tos_status_is_up(self):
+        response = self.client.get('/terms')
         self.assertEqual(response.status_code, 200)
